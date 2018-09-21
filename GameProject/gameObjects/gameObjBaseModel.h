ccb22fa13f1b7174f0ae87dcb11fec9e25f604a1
spaceperson <mawebbe1@asu.edu>
2018-09-21 12:12:56 -0700

    Updated title description and date

--- GameProject/gameObjects/gameObjBaseModel.h
+++ GameProject/gameObjects/gameObjBaseModel.h
@@ -45,8 +45,10 @@ void initBaseModels() {
 
 	for (int i = 0; i < 4; i++) {
 		for (int j = 0; j < 4; j++) {
-			baseModels[baseModelAmount - 1] = newBaseModel(-8 + i * 4.0, 0.0f, -20.0f + j * 4.0, 0.0f, 180.0f, 0.0f, 400.0, 6);
-			baseModels[baseModelAmount - 1] = newBaseModel(-8 + i * 4.0, 0.0f, -20.0f + j * 4.0, 0.0f, 180.0f, 0.0f, 400.0, 7);
+			if (j % 2 == 0 && i % 2 == 0) {
+				baseModels[baseModelAmount - 1] = newBaseModel(-8 + i * 4.0, 0.0f, -20.0f + j * 4.0, 0.0f, 180.0f, 0.0f, 400.0, 6); // Tree Models
+			}
+			baseModels[baseModelAmount - 1] = newBaseModel(-8 + i * 4.0, 0.0f, -20.0f + j * 4.0, 0.0f, 180.0f, 0.0f, 400.0, 7); // Grass Models
 		}
 	}
 
