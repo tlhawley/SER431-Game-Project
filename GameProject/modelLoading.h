e210027569aea687d15565a4c8e296cb0e4a3c6f
tlhawley <tlhawley@asu.edu>
2018-09-21 22:09:31 -0700

    modelLoading.h Update

--- GameProject/modelLoading.h
+++ GameProject/modelLoading.h
@@ -54,9 +54,25 @@ GLuint meshTree;
 GLuint meshGrassCluster;
 
 
+
+int uniqueMeshID = 0;
+
 void loadModels();
 void drawGear1();
 void drawGear2();
+
+
+GLuint loadObj(char const* file, GLuint textureID);
+
+// Function used to simplify loading objects and reduce repetitive code
+GLuint loadObj(char const* file, GLuint textureID) {
+	Mesh* aMesh = loadFile(file);
+	if (aMesh == NULL) exit(1);
+	uniqueMeshID = uniqueMeshID + 1;
+	return meshToDisplayList(aMesh, uniqueMeshID, textureID);
+}
+
+
 
 void loadModels() {
 
@@ -74,176 +90,73 @@ void loadModels() {
 	texture_from_file(&texture_array[10], "./src/textures/TreeTexture.bmp");
 	texture_from_file(&texture_array[11], "./src/textures/GrassClusterTexture.bmp");
 
-	//Mesh* mesh1 = loadFile("./src/obj files/Object Thing2.obj");
-	Mesh* mesh1 = loadFile("./src/obj files/Map20.obj");
-	if (mesh1 == NULL) exit(1);
-	displayListMaterialTest1 = meshToDisplayList(mesh1, 1, texture_array[0]);
 
-	Mesh* mesh2 = loadFile("./src/obj files/ClockFrame.obj");
-	if (mesh2 == NULL) exit(1);
-	displayListClockFrame = meshToDisplayList(mesh2, 2, texture_array[0]);
+	displayListMaterialTest1 = loadObj("./src/obj files/Map20.obj", texture_array[0]);
 
-	Mesh* mesh3 = loadFile("./src/obj files/ClockWhite.obj");
-	if (mesh3 == NULL) exit(1);
-	displayListClockWhite = meshToDisplayList(mesh3, 3, texture_array[0]);
+	displayListClockFrame = loadObj("./src/obj files/ClockFrame.obj", texture_array[0]);
 
+	displayListClockWhite = loadObj("./src/obj files/ClockWhite.obj", texture_array[0]);
 
-	Mesh* mesh4 = loadFile("./src/obj files/Spikes2.obj");
-	if (mesh4 == NULL) exit(1);
-	meshSpikes = meshToDisplayList(mesh4, 4, texture_array[2]);
+	meshSpikes = loadObj("./src/obj files/Spikes2.obj", texture_array[2]);
 
+	meshDoorFrame = loadObj("./src/obj files/DoorFrame3.obj", texture_array[6]);
 
+	meshDoorLeft = loadObj("./src/obj files/DoorRight3.obj", texture_array[5]);
 
-	Mesh* mesh5 = loadFile("./src/obj files/DoorFrame3.obj");
-	if (mesh5 == NULL) exit(1);
-	meshDoorFrame = meshToDisplayList(mesh5, 5, texture_array[6]);
+	meshDoorRight = loadObj("./src/obj files/DoorLeft3.obj", texture_array[5]);
 
-	Mesh* mesh6 = loadFile("./src/obj files/DoorRight3.obj");
-	if (mesh6 == NULL) exit(1);
-	meshDoorLeft = meshToDisplayList(mesh6, 6, texture_array[5]);
+	meshDoorLock = loadObj("./src/obj files/DoorLock3.obj", texture_array[0]);
 
-	Mesh* mesh7 = loadFile("./src/obj files/DoorLeft3.obj");
-	if (mesh7 == NULL) exit(1);
-	meshDoorRight = meshToDisplayList(mesh7, 7, texture_array[5]);
+	meshRoomWall1 = loadObj("./src/obj files/RoomWall1Colored.obj", texture_array[0]);
 
-	Mesh* mesh8 = loadFile("./src/obj files/DoorLock3.obj");
-	if (mesh8 == NULL) exit(1);
-	meshDoorLock = meshToDisplayList(mesh8, 8, texture_array[0]);
+	meshRoomWall2 = loadObj("./src/obj files/RoomWall2Colored.obj", texture_array[0]);
 
+	meshBox = loadObj("./src/obj files/box.obj", texture_array[0]);
 
-	//RoomWall1Colored
-	Mesh* mesh21 = loadFile("./src/obj files/RoomWall1Colored.obj");
-	if (mesh21 == NULL) exit(1);
-	meshRoomWall1 = meshToDisplayList(mesh21, 21, texture_array[0]);
+	meshUIMap = loadObj("./src/obj files/Map18_map.obj", texture_array[0]);
 
-	Mesh* mesh22 = loadFile("./src/obj files/RoomWall2Colored.obj");
-	if (mesh22 == NULL) exit(1);
-	meshRoomWall2 = meshToDisplayList(mesh22, 22, texture_array[0]);
+	meshGem = loadObj("./src/obj files/Gem.obj", texture_array[0]);
 
+	meshKey = loadObj("./src/obj files/Key.obj", texture_array[0]);
 
+	meshPorsche = loadObj("./src/obj files/porsche.obj", texture_array[0]);
 
+	meshAL = loadObj("./src/obj files/al.obj", texture_array[0]);
 
-	Mesh* mesh9 = loadFile("./src/obj files/box.obj");
-	if (mesh9 == NULL) exit(1);
-	meshBox = meshToDisplayList(mesh9, 9, texture_array[0]);
+	meshGear1 = loadObj("./src/obj files/Gear1.obj", texture_array[0]);
 
-	//Mesh* mesh10 = loadFile("./src/obj files/TheMap.obj");
-	Mesh* mesh10 = loadFile("./src/obj files/Map18_map.obj");
-	if (mesh10 == NULL) exit(1);
-	meshUIMap = meshToDisplayList(mesh10, 10, texture_array[0]);
+	meshGear2 = loadObj("./src/obj files/Gear2.obj", texture_array[0]);
 
+	meshPortal = loadObj("./src/obj files/Portal.obj", texture_array[0]);
 
-
-
-
-
-	Mesh* mesh31 = loadFile("./src/obj files/Gem.obj");
-	if (mesh31 == NULL) exit(1);
-	meshGem = meshToDisplayList(mesh31, 31, texture_array[0]);
-
-	Mesh* mesh32 = loadFile("./src/obj files/Key.obj");
-	if (mesh32 == NULL) exit(1);
-	meshKey = meshToDisplayList(mesh32, 32, texture_array[0]);
-
-	Mesh* mesh33 = loadFile("./src/obj files/porsche.obj");
-	if (mesh33 == NULL) exit(1);
-	meshPorsche = meshToDisplayList(mesh33, 33, texture_array[0]);
-
-	Mesh* mesh34 = loadFile("./src/obj files/al.obj");
-	if (mesh34 == NULL) exit(1);
-	meshAL = meshToDisplayList(mesh34, 34, texture_array[0]);
-
-
-
-
-
-	Mesh* mesh35 = loadFile("./src/obj files/Gear1.obj");
-	if (mesh35 == NULL) exit(1);
-	meshGear1 = meshToDisplayList(mesh35, 35, texture_array[0]);
-
-	Mesh* mesh36 = loadFile("./src/obj files/Gear2.obj");
-	if (mesh36 == NULL) exit(1);
-	meshGear2 = meshToDisplayList(mesh36, 36, texture_array[0]);
-
-	Mesh* mesh40 = loadFile("./src/obj files/Portal.obj");
-	if (mesh40 == NULL) exit(1);
-	meshPortal = meshToDisplayList(mesh40, 40, texture_array[0]);
-
-	/*
-	Mesh* mesh41 = loadFile("./src/obj files/PortalE.obj");
-	if (mesh41 == NULL) exit(1);
-	meshPortalE = meshToDisplayList(mesh41, 41);
-
-	Mesh* mesh42 = loadFile("./src/obj files/PortalGlow.obj");
-	if (mesh42 == NULL) exit(1);
-	meshPortalGlow = meshToDisplayList(mesh42, 42);
-	*/
-
+	meshFloor = loadObj("./src/obj files/Map18_Floor.obj", texture_array[3]);
 	
-	Mesh* mesh41 = loadFile("./src/obj files/Map18_Floor.obj");
-	if (mesh41 == NULL) exit(1);
-	meshFloor = meshToDisplayList(mesh41, 41, texture_array[3]);
+	meshWall = loadObj("./src/obj files/Map18Wall.obj", texture_array[4]);
+
+	meshBench = loadObj("./src/obj files/furniture/Bench.obj", texture_array[7]);
+
+	meshChair = loadObj("./src/obj files/furniture/Chair.obj", texture_array[7]);
+
+	hangingLamp = loadObj("./src/obj files/furniture/hangingLamp.obj", texture_array[0]);
 	
-	Mesh* mesh42 = loadFile("./src/obj files/Map18Wall.obj");
-	if (mesh42 == NULL) exit(1);
-	meshWall = meshToDisplayList(mesh42, 42, texture_array[4]);
+	hangingLampE = loadObj("./src/obj files/furniture/hangingLampE.obj", texture_array[0]);
 
+	meshCouch = loadObj("./src/obj files/furniture/Couch.obj", texture_array[0]);
 
+	meshDesk = loadObj("./src/obj files/furniture/Desk.obj", texture_array[7]);
 
-	Mesh* mesh43 = loadFile("./src/obj files/furniture/Bench.obj");
-	if (mesh43 == NULL) exit(1);
-	meshBench = meshToDisplayList(mesh43, 43, texture_array[7]);
+	meshSkyBox = loadObj("./src/obj files/CubeMap.obj",texture_array[9]);
 
-	Mesh* mesh44 = loadFile("./src/obj files/furniture/Chair.obj");
-	if (mesh44 == NULL) exit(1);
-	meshChair = meshToDisplayList(mesh44, 44, texture_array[7]);
+	meshABox = loadObj("./src/obj files/aBox.obj", texture_array[1]);
 
-	Mesh* mesh45 = loadFile("./src/obj files/furniture/hangingLamp.obj");
-	if (mesh45 == NULL) exit(1);
-	hangingLamp = meshToDisplayList(mesh45, 45, texture_array[0]);
+	meshq3MirrorRoom = loadObj("./src/obj files/q3MirrorRoom.obj", texture_array[3]);
 
-	Mesh* mesh46 = loadFile("./src/obj files/furniture/hangingLampE.obj");
-	if (mesh46 == NULL) exit(1);
-	hangingLampE = meshToDisplayList(mesh46, 46, texture_array[0]);
+	meshq3MirrorPlane = loadObj("./src/obj files/q3MirrorPlane.obj", texture_array[3]);
 
-	Mesh* mesh47 = loadFile("./src/obj files/furniture/Couch.obj");
-	if (mesh47 == NULL) exit(1);
-	meshCouch = meshToDisplayList(mesh47, 47, texture_array[0]);
+	meshTree = loadObj("./src/obj files/LowPolyTree.obj", texture_array[10]);
 
-	Mesh* mesh48 = loadFile("./src/obj files/furniture/Desk.obj");
-	if (mesh48 == NULL) exit(1);
-	meshDesk = meshToDisplayList(mesh48, 48, texture_array[7]);
+	meshGrassCluster = loadObj("./src/obj files/GrassCluster.obj", texture_array[11]);
 
-
-
-
-	Mesh* mesh49 = loadFile("./src/obj files/CubeMap.obj");
-	if (mesh49 == NULL) exit(1);
-	meshSkyBox = meshToDisplayList(mesh49, 49, texture_array[9]);
-
-
-	Mesh* mesh50 = loadFile("./src/obj files/aBox.obj");
-	if (mesh50 == NULL) exit(1);
-	meshABox = meshToDisplayList(mesh50, 50, texture_array[1]);
-
-	Mesh* mesh51 = loadFile("./src/obj files/q3MirrorRoom.obj");
-	if (mesh51 == NULL) exit(1);
-	meshq3MirrorRoom = meshToDisplayList(mesh51, 51, texture_array[3]);
-
-
-	Mesh* mesh52 = loadFile("./src/obj files/q3MirrorPlane.obj");
-	if (mesh52 == NULL) exit(1);
-	meshq3MirrorPlane = meshToDisplayList(mesh52, 52, texture_array[3]);
-
-
-	Mesh* mesh53 = loadFile("./src/obj files/LowPolyTree.obj");
-	if (mesh53 == NULL) exit(1);
-	meshTree = meshToDisplayList(mesh53, 53, texture_array[10]);
-
-
-	Mesh* mesh54 = loadFile("./src/obj files/GrassCluster.obj");
-	if (mesh54 == NULL) exit(1);
-	meshGrassCluster = meshToDisplayList(mesh54, 54, texture_array[11]);
 
 }
 
